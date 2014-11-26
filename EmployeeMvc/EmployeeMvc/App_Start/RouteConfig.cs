539134818ca4852e016ef3e89fcf411637fba065
Geetika Batra <geetika791@gmail.com>
2014-11-26 14:01:47 +0530

    Main Page

--- EmployeeMvc/EmployeeMvc/App_Start/RouteConfig.cs
+++ EmployeeMvc/EmployeeMvc/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace EmployeeMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
