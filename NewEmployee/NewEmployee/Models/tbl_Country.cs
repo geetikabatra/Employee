c9e93f6201ede66e35c37a21b39e47c172fa57ff
Geetika Batra <geetika791@gmail.com>
2015-01-03 00:44:19 +0530

    Creating Model Classes

---
+++ NewEmployee/NewEmployee/Models/tbl_Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewEmployee.Models
+{
+    public class tbl_Country
+    {
+        public int cou_Id { get; set; }
+        public string cou_Name { get; set; }
+
+        public virtual tbl_city tbl_city { get; set; }
+
+    }
+}
